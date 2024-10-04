76619031e6885c89ec2d3413b623475ce62701ed
Anna Abadi <faeghe.hajiabadi@gmail.com>
2024-10-04 16:26:38 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Die from "./components/Die";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+    }
+    return newDice;
+  }
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+function holdDice(id){
+  
+  setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+}
+  const diceElement = dice.map((die) => {
+    return (
+      <Die
+        key={die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        holdDice={() => holdDice(die.id)}
+      />
+    );
+  });
+  function rollDice() {
+    setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="dice-container">{diceElement}</div>
+      <button className="rolle-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
   );
 }
 
